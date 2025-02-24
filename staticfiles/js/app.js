06d1ec55eec91b0fbfa6019f9cb1ef363ce32c89
Javidan <iamthejacks0l0@gmail.com>
2025-02-24 17:54:56 +0400

    modified:   app/__pycache__/settings.cpython-313.pyc 	modified:   app/__pycache__/wsgi.cpython-313.pyc 	modified:   staticfiles/css/style.css 	modified:   staticfiles/css/style.css.map 	modified:   staticfiles/css/style.scss 	new file:   staticfiles/font/MuliLight-lg9VZ.ttf 	modified:   staticfiles/js/app.js 	new file:   staticfiles/media/bk1.jpg 	modified:   templates/addons/header.html 	modified:   templates/addons/nav.html

--- staticfiles/js/app.js
+++ staticfiles/js/app.js
@@ -1,10 +1,17 @@
-$('.headCarusel').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots:false,
-    speed: 7000,
-    arrows:false,
+window.addEventListener("scroll", function () {
+    let navbar = document.querySelector(".navbarBlock");
+    let logoImgTop = document.querySelector(".logoImgTop");
+    if (window.scrollY > 10) {
+        navbar.style.padding = "1.5vw 3.5vw";
+        navbar.style.position = "fixed";
+        navbar.style.top = "0";
+        navbar.style.width = "100%";
+        logoImgTop.style.width = "12.5vw";
+    } else {
+        navbar.style.padding = "";
+        navbar.style.position = "";
+        navbar.style.width = "";
+        navbar.style.top = "";
+        logoImgTop.style.width = "";
+    }
 });
