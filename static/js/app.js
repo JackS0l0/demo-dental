2e76c25c8f0d9efb8312a4927d69cee022cdedc0
Javidan <iamthejacks0l0@gmail.com>
2025-02-25 05:16:18 +0400

    modified:   static/css/style.css 	modified:   static/css/style.css.map 	modified:   static/css/style.scss 	new file:   static/font/MuliLight-lg9VZ.ttf 	modified:   static/js/app.js 	new file:   static/media/bk1.jpg 	new file:   static/media/bk2.jpg 	new file:   static/media/bk3.jpg 	new file:   static/media/bk4.jpg 	new file:   static/media/f.png 	new file:   static/media/g.png 	new file:   static/media/google.png 	new file:   static/media/partner.png 	new file:   static/media/x.png 	modified:   staticfiles/css/style.css 	modified:   staticfiles/css/style.css.map 	modified:   staticfiles/css/style.scss 	modified:   staticfiles/js/app.js 	new file:   staticfiles/media/bk4.jpg 	new file:   staticfiles/media/google.png 	new file:   staticfiles/media/partner.png 	modified:   templates/base.html 	modified:   templates/index.html

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,30 @@
-$('.headCarusel').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots:false,
-    speed: 7000,
-    arrows:false,
+window.addEventListener("scroll", function () {
+    let navbar = document.querySelector(".navbarBlock");
+    let logoImgTop = document.querySelector(".logoImgTop");
+    if (window.scrollY > 10) {
+        navbar.style.padding = "1.5vw 3.5vw";
+        navbar.style.position = "fixed";
+        navbar.style.top = "0";
+        navbar.style.width = "100%";
+        logoImgTop.style.width = "12.5vw";
+    } else {
+        navbar.style.padding = "";
+        navbar.style.position = "";
+        navbar.style.width = "";
+        navbar.style.top = "";
+        logoImgTop.style.width = "";
+    }
 });
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
--- staticfiles/js/app.js
+++ staticfiles/js/app.js
@@ -15,3 +15,16 @@ window.addEventListener("scroll", function () {
         logoImgTop.style.width = "";
     }
 });
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
